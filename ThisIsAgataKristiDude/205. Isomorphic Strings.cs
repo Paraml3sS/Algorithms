8f942520d5026073d69516d4d24d178ca378dda8
Yurii Budzyn <yura.budz@gmail.com>
2023-11-14 21:49:53 +0200

    Add project files.

---
+++ ThisIsAgataKristiDude/205. Isomorphic Strings.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/isomorphic-strings/
+/// 
+/// Faster solutions are by checking unique key and value on the go instead of Count Distinct
+
+namespace IsomorphicStrings
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData("egg", "add", true)]
+        [InlineData("foo", "bar", false)]
+        [InlineData("paper", "title", true)]
+        [InlineData("egg", "aaa", false)]
+        [InlineData("egg", "dad", false)]
+
+        public void SolutionTest(string s, string t, bool realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var batShit = dude.SolveThisShitBroooo(s, t);
+
+            batShit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public bool SolveThisShitBroooo(string s, string t)
+        {
+            Dictionary<char, char> letterPairs = new(s.Length);
+
+            string transformedString = string.Empty;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                letterPairs.TryAdd(s[i], t[i]);
+                transformedString += letterPairs[s[i]];
+            }
+
+            if (letterPairs.Values.Count() != letterPairs.Values.Distinct().Count())
+            {
+                return false;
+            }
+
+            return transformedString == t;
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/206. Reverse Linked List.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ReverseLinkedList
+{
+    public class MakeItWorkDude
+    {
+        [Fact]
+        public void FullArray()
+        {
+            ThisIsAlgoDude dude = new();
+
+            ListNode node4 = new(4);
+            ListNode node3 = new(3, node4);
+            ListNode node2 = new(2, node3);
+            ListNode node1 = new(1, node2);
+            ListNode node0 = new(0, node1);
+
+            var reversed = dude.SolveThisShitBroooo(node0);
+
+            reversed.val.Should().Be(4);
+            reversed.next.val.Should().Be(3);
+            reversed.next.next.val.Should().Be(2);
+            reversed.next.next.next.val.Should().Be(1);
+            reversed.next.next.next.next.val.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void NoElements()
+        {
+            ThisIsAlgoDude dude = new();
+
+            var reversed = dude.SolveThisShitBroooo(null);
+
+            reversed.Should().BeNull();
+        }
+
+        [Fact]
+        public void OneElement()
+        {
+            ThisIsAlgoDude dude = new();
+
+            var reversed = dude.SolveThisShitBroooo(new ListNode());
+
+            reversed.val.Should().Be(0);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public ListNode SolveThisShitBroooo(ListNode head)
+        {
+            return ShawtyHelp(head, head?.next);
+        }
+
+        public ListNode ShawtyHelp(ListNode head, ListNode nextNode)
+        {
+            if (head is null || nextNode is null)
+            {
+                return head;
+            }
+
+            var afterNext = nextNode.next;
+            nextNode.next = head;
+
+            return ShawtyHelp(nextNode, afterNext);
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+          this.val = val;
+          this.next = next;
+        }
+    }
+}
