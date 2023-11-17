eb7041e789fdd0ed60fb819f28f07e0bb53456ac
Yurii Budzyn <yura.budz@gmail.com>
2023-11-17 15:11:12 +0200

    +2

---
+++ ThisIsAgataKristiDude/125. Valid Palindrome.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/valid-palindrome
+/// 
+/// same as best solution
+
+namespace ValidPalindrome
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("race a car", false)]
+        [InlineData(" ", true)]
+        [InlineData("aa", true)]
+
+        public void SolutionTest(string s, bool realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var batShit = dude.SolveThisShitBroooo(s);
+
+            batShit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public bool SolveThisShitBroooo(string s)
+        {
+            for ( int i = 0, j = s.Length - 1; i < j; ) {
+
+                if (char.IsAsciiLetterOrDigit(s[i]) == false)
+                {
+                    ++i; continue;
+                }
+
+                if (char.IsAsciiLetterOrDigit(s[j]) == false)
+                {
+                    --j; continue;
+                }
+
+                if (
+                    char.ToLower(s[i]) != char.ToLower(s[j])
+                    )
+                {
+                    return false;
+                }
+
+                ++i; --j;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/27. Remove Element.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/remove-element
+/// 
+/// No need to swap, element can be just removed/cleaned etc...
+
+namespace RemoveElement
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, new int[] { 2, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, new int[] { 0, 0, 1, 3, 4 })]
+        public void SolutionTest(int[] nums, int val, int[] realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbSHit = dude.SolveThisShitBroooo(nums, val);
+
+            dumbSHit.Should().Be(realShit.Length);
+            nums.Take(dumbSHit).Order().Should().BeEquivalentTo(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(int[] nums, int val)
+        {
+            int k = 0;
+            int j = nums.Length - 1;
+
+            for (int i = 0; i <= j;)
+            {
+                if (nums[i] == val && val == nums[j])
+                {
+                    --j; ++k;
+                }
+                else if (nums[i] == val)
+                {
+                    Swap(nums, val, j, i);
+                    ++i; --j; ++k;
+                }
+                else ++i;
+            }
+
+            return nums.Length - k;
+        }
+
+        private static void Swap(int[] nums, int val, int j, int i)
+        {
+            var elementFromTheEnd = nums[j];
+            nums[j] = val;
+            nums[i] = elementFromTheEnd;
+        }
+    }
+}
