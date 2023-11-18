d0b64b188700ae6558871ac4dbb85bbd2a27f7c8
Yurii Budzyn <yura.budz@gmail.com>
2023-11-18 14:45:16 +0200

    +2

---
+++ ThisIsAgataKristiDude/104. Maximum Depth of Binary Tree.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/maximum-depth-of-binary-tree
+/// 
+///
+
+namespace MaximumDepthOfBinaryTree
+{
+    public class MakeItWorkDude
+    {
+        [Fact]
+        public void SolutionTest()
+        {
+            TreeNode node3 = new(15);
+            TreeNode node4 = new(7);
+
+            TreeNode node1 = new(9);
+            TreeNode node2 = new(20, node3, node4);
+
+            TreeNode root = new(3, node1, node2);
+
+
+            ThisIsAlgoDude dude = new();
+
+            var dumbSHit = dude.SolveThisShitBroooo(root);
+
+            dumbSHit.Should().Be(3);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(TreeNode root)
+        {
+            return ShawtyHelp(level: 0, root);
+        }
+
+        public int ShawtyHelp(int level, TreeNode root = null)
+        {
+            if (root is null) return level;
+
+            ++level;
+
+            int leftPathLevel = ShawtyHelp(level, root.left);
+            int rightPathLevel = ShawtyHelp(level, root.right);
+
+            return Math.Max(leftPathLevel, rightPathLevel);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/66. Plus One.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/plus-one
+/// 
+///
+
+namespace PlusOne
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+        [InlineData(new int[] { 1, 2, 9 }, new int[] { 1, 3, 0 })]
+        [InlineData(new int[] { 1, 9, 9 }, new int[] { 2, 0, 0 })]
+        [InlineData(new int[] { 9 }, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0 }, new int[] { 1 })]
+        [InlineData(new int[] { 9, 9, 9, 9 }, new int[] { 1, 0, 0, 0, 0 })]
+        [InlineData(new int[] { 8, 9, 9, 9 }, new int[] { 9, 0, 0, 0 })]
+        public void SolutionTest(int[] nums, int[] realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbSHit = dude.SolveThisShitBroooo(nums);
+
+            dumbSHit.Should().Equal(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int[] SolveThisShitBroooo(int[] digits)
+        {
+
+            for (int j = digits.Length - 1; j >= 0; j--)
+            {
+                if (digits[j] + 1 > 9)
+                {
+                    digits[j] = 0;
+                }
+                else
+                {
+                    digits[j] += 1;
+                    return digits;
+                }
+            }
+
+            int[] newArray = new int[digits.Length + 1];
+            newArray[0] = 1;
+
+            return newArray;
+        }
+    }
+}
