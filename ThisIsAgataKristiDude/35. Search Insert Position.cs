df69dab4e54f231ff59984b6f0710375d856906b
Yurii Budzyn <yura.budz@gmail.com>
2023-11-20 19:22:48 +0200

    +2

---
+++ ThisIsAgataKristiDude/35. Search Insert Position.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/search-insert-position
+/// 
+/// 
+
+namespace SearchInsertPosition
+
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 5, 2)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 2, 1)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 7, 4)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 0, 0)]
+        [InlineData(new int[] { 1 }, 2, 1)]
+        public void Solution(int[] nums, int target, int realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbShit = dude.SolveThisShitBroooo(nums, target);
+
+            dumbShit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(int[] nums, int target)
+        {
+
+
+
+
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] == target)
+            //    {
+            //        return i;
+            //    }
+
+            //    if (target < nums[i])
+            //    {
+            //        return i;
+            //    }
+            //}
+
+            //if (target > nums[nums.Length - 1])
+            //{
+            //    return nums.Length;
+            //}
+
+            return ShawtyHelp(nums, target, 0, nums.Length - 1);
+        }
+
+        public int ShawtyHelp(int[] nums, int target, int begin, int end)
+        {
+            if (begin >= end)
+                return target > nums[end] ? end + 1 : begin;
+
+            int middle = begin + (end - begin) / 2;
+
+            if (target == nums[middle])
+            {
+                return middle;
+            }
+            else if (target < nums[middle])
+            {
+                return ShawtyHelp(nums, target, begin, middle);
+            }
+            else
+            {
+                return ShawtyHelp(nums, target, middle + 1, end);
+            }
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/70. Climbing Stairs.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/climbing-stairs/
+/// 
+///
+
+namespace ClimbingStairs
+{
+    public class MakeItWorkDude
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(5, 8)]
+        [InlineData(6, 13)]
+        public void Solution(int n, int realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbShit = dude.SolveThisShitBroooo(n);
+
+            dumbShit.Should().Be(realShit);
+        }
+    }
+
+
+    /// <summary>
+    /// 1 - 1
+    /// 1
+    /// 
+    /// 1 level
+    /// 
+    /// 2 - 2
+    /// 1 1
+    /// 2
+    /// 
+    /// 2 levels
+    /// 
+    /// 3 - 3
+    /// 1 1 1
+    /// 1 2
+    /// 2 1
+    /// 
+    /// 3 levels
+    /// 
+    /// 4 - 5
+    /// 1 1 1 1
+    /// 1 1 2
+    /// 2 1 1
+    /// 1 2 1
+    /// 2 2
+    /// 
+    /// 3 levels
+    /// 
+    /// 5 - 8
+    /// 1 1 1 1 1
+    /// 1 1 1 2
+    /// 1 1 2 1
+    /// 1 2 1 1
+    /// 2 1 1 1
+    /// 2 2 1
+    /// 2 1 2
+    /// 1 1 2
+    /// 
+    /// 
+    /// 6 - 13
+    /// 1 1 1 1 1 1
+    /// 1 1 1 1 2
+    /// 1 1 1 2 1
+    /// 1 1 2 1 1
+    /// 1 2 1 1 1
+    /// 2 1 1 1 1
+    /// 2 2 1 1
+    /// 2 1 2 1
+    /// 2 1 1 2
+    /// 1 2 2 1
+    /// 1 2 1 2
+    /// 1 1 2 2
+    /// 2 2 2
+    /// 
+    /// </summary>
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 2;
+            }
+
+            return ShawtyHelp(previous: 1, current: 2, n, currentLevel: 2);
+
+            //int previousLevel = 1, currentLevel = 2;
+            //for (int i = 2; i < n; i++)
+            //{
+            //    int temp = currentLevel;
+            //    currentLevel += previousLevel;
+            //    previousLevel = temp;
+            //}
+
+
+            //return currentLevel;
+        }
+
+        public int ShawtyHelp(int previous, int current, int n, int currentLevel = 2)
+        {
+            if (currentLevel == n)
+                return current;
+
+            return ShawtyHelp(current, current + previous, n, ++currentLevel);
+        }
+    }
+}
