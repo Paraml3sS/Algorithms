ba1f9eed71f06caaf2aedaeecfbc9976a56567e3
Yurii Budzyn <yura.budz@gmail.com>
2023-11-21 22:00:05 +0200

    +2

---
+++ ThisIsAgataKristiDude/108. Convert Sorted Array to Binary Search Tree.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+
+/// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree
+/// 
+/// Optimize by returnning treeNode (null) instead of void - so ifs not needed
+/// ref is not needed too
+
+namespace ConvertSortedArrayToBinarySearchTree
+{
+    public class MakeItWorkDude
+    {
+
+        [Theory]
+        [InlineData(new int[] { -10, -3, 0, 5, 9 })]
+        [InlineData(new int[] { 1, 3})]
+        public void SolutionTest(int[] nums)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbShit = dude.SolveThisShitBroooo(nums);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public TreeNode SolveThisShitBroooo(int[] nums)
+        {
+            int numOfNodes = 0;
+            TreeNode root = new();
+            ShawtyHelp(nums, 0, nums.Length, ref numOfNodes, root);
+
+            return root;
+        }
+
+        public void ShawtyHelp(int[] nums, int i, int j, ref int numOfNodes, TreeNode root = null)
+        {
+            if (numOfNodes++ == nums.Length)
+                return;
+                
+
+            int middle = (i + j) / 2;
+            root.val = nums[middle];
+
+            if (j - i <= 1)
+                return;
+            
+
+            if (i < middle)
+            {
+                root.left = new TreeNode();
+                ShawtyHelp(nums, i, middle, ref numOfNodes, root.left);
+            }
+
+            if (middle + 1 < j)
+            {
+                root.right = new TreeNode();
+                ShawtyHelp(nums, middle + 1, j, ref numOfNodes, root.right);
+            }
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/69. Sqrt(x).cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/sqrtx
+/// 
+/// Binary search
+
+namespace SqrtX
+{
+    public class MakeItWorkDude
+    {
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(3, 1)]
+        [InlineData(8, 2)]
+        [InlineData(4, 2)]
+        [InlineData(1025, 32)]
+        [InlineData(2147395599, 46340)]
+        public void SolutionTest(int x, int realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbSHit = dude.SolveThisShitBroooo(x);
+
+            dumbSHit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(int x)
+        {
+           
+            int left = 1, right = x, middle;
+
+            while (true)
+            {
+                middle = left + (right - left) / 2;
+
+                if (middle <= x / middle)
+                {
+                    if (middle + 1 > x / (middle + 1))
+                        return middle;
+
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+        }
+    }
+}
