306e280b02c69ce9b3fd755ad224a7457a935b5c
Yurii Budzyn <yura.budz@gmail.com>
2023-11-22 21:39:53 +0200

    +2

---
+++ ThisIsAgataKristiDude/Easy/14. Longest Common Prefix.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/longest-common-prefix
+/// 
+/// Lexicographic sorting allows to compare first and last
+/// mine can be simplified usiing foreach instead of for
+
+namespace LongestCommonPrefix
+{
+    public class MakeItWorkDude
+    {
+
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "flower", "flower", "flower" }, "flower")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new string[] { "" }, "")]
+        [InlineData(new string[] { "", "" }, "")]
+        [InlineData(new string[] { "a" }, "a")]
+        [InlineData(new string[] { "ab", "a" }, "a")]
+        public void SolutionTest(string[] strs, string realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbshit = dude.SolveThisShitBroooo(strs);
+
+            dumbshit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public string SolveThisShitBroooo(string[] strs)
+        {
+            if (strs.Length == 1 || string.IsNullOrEmpty(strs[0]))
+                return strs.First();
+
+            int maxPrefixLength = strs.First().Length;
+
+            char prevChar = strs[0][0];
+
+            int prefixPosition = 0;
+
+            for (int i = 0; ; i++)
+            {
+                if (string.IsNullOrEmpty(strs[i]) || strs[i][prefixPosition] != prevChar)
+                    break;
+
+                prevChar = strs[i][prefixPosition];
+
+                if (prefixPosition == 0 && strs[i].Length < maxPrefixLength)
+                    maxPrefixLength = strs[i].Length;
+
+                if (i >= strs.Length - 1)
+                {
+                    if (++prefixPosition >= maxPrefixLength)
+                        break;
+
+                    i = 0; 
+
+                    prevChar = strs[i][prefixPosition];
+
+                }
+            }
+
+
+            return strs.First().Substring(0, prefixPosition);
+        }
+    }
+}
---
+++ ThisIsAgataKristiDude/Easy/58. Length of Last Word.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+/// https://leetcode.com/problems/length-of-last-word/
+/// 
+///
+
+namespace LengthOfLastWord
+{
+    public class MakeItWorkDude
+    {
+
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        [InlineData("a", 1)]
+        public void SolutionTest(string s, int realShit)
+        {
+            ThisIsAlgoDude dude = new();
+
+            var dumbShit = dude.SolveThisShitBroooo(s);
+
+            dumbShit.Should().Be(realShit);
+        }
+    }
+
+
+    public class ThisIsAlgoDude
+    {
+        public int SolveThisShitBroooo(string s)
+        {
+            int count = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (char.IsLetter(s[i])) count++;
+
+                if (count > 0 && (i == 0 || char.IsWhiteSpace(s[i - 1])))
+                    break;
+            }
+
+
+            return count;
+        }
+    }
+}
